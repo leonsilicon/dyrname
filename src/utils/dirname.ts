2ca3f9a2b4d2e78a5b91e60861b42efb41117d7d
Leon <leon@leondreamed.com>
2023-07-17 22:02:07 -0700

    fix: esm

--- src/utils/dirname.ts
+++ src/utils/dirname.ts
@@ -1,16 +1,20 @@
+import path from "path";
+import os from "os";
+
 export function dirname() {
-  let path = /(?<path>[^\(\s]+):[0-9]+:[0-9]+/.exec(
+  let filepath = /(?<path>[^\(\s]+):[0-9]+:[0-9]+/.exec(
     new Error().stack!.split("\n").slice(2, 3)[0]!
   )!.groups!.path!;
 
-  if (path.indexOf("file") >= 0) {
-    path = new URL(path).pathname;
+  if (filepath.indexOf("file") >= 0) {
+    filepath = new URL(filepath).pathname;
   }
-  let dirname = require("path").dirname(path);
+  let dirname = path.dirname(filepath);
   try {
-    if (dirname[0] === "/" && require("os").platform() === "win32") {
+    if (dirname[0] === "/" && os.platform() === "win32") {
       dirname = dirname.slice(1);
     }
   } catch (_) {}
+
   return dirname;
 }
--- src/utils/filename.ts
+++ src/utils/filename.ts
@@ -1,14 +1,16 @@
+import os from 'os'
+
 export function filename() {
-  let path = /(?<path>[^\(\s]+):[0-9]+:[0-9]+/.exec(
+  let filepath = /(?<path>[^\(\s]+):[0-9]+:[0-9]+/.exec(
     new Error().stack!.split("\n").slice(2, 3)[0]!
   )!.groups!.path!;
 
-  if (path.indexOf("file") >= 0) {
-    path = new URL(path).pathname;
+  if (filepath.indexOf("file") >= 0) {
+    filepath = new URL(filepath).pathname;
   }
-  let filename = path;
+  let filename = filepath;
   try {
-    if (filename[0] === "/" && require("os").platform() === "win32") {
+    if (filename[0] === "/" && os.platform() === "win32") {
       filename = filename.slice(1);
     }
   } catch (_) {}
